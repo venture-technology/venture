868a82fc828bb7233b11a2164d4291528c43f54d
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-08-07 23:46:04 -0300

    feat: base partner

---
+++ internal/controller/partner_controller.go
@@ -0,0 +1,36 @@
+package controller
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/venture-technology/venture/internal/service"
+)
+
+type PartnerController struct {
+	partnerservice *service.PartnerService
+}
+
+func NewPartnerService(partnerservice *service.PartnerService) *PartnerController {
+	return &PartnerController{
+		partnerservice: partnerservice,
+	}
+}
+
+func (ct *PartnerController) RegisterRoutes(router *gin.Engine) {
+	api := router.Group("api/v1/partner")
+
+	api.GET("/driver", ct.GetPartnersByDriver) // para que o motorista busque todos seus parceiros
+	api.GET("/school", ct.GetPartnersBySchool) // para que a escola busque todos seus parceiros
+	api.DELETE("/:cnh", ct.DeletePartner)      // para que a escola exclua um parceiro da sua rede
+}
+
+func (ct *PartnerController) GetPartnersByDriver(c *gin.Context) {
+
+}
+
+func (ct *PartnerController) GetPartnersBySchool(c *gin.Context) {
+
+}
+
+func (ct *PartnerController) DeletePartner(c *gin.Context) {
+
+}
---
+++ internal/repository/partner_repository.go
@@ -0,0 +1,46 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+
+	"github.com/venture-technology/venture/models"
+)
+
+type IPartnerRepository interface {
+	IsPartner(ctx context.Context, partner *models.Partner) (bool, error)
+	CreatePartners(ctx context.Context, partner *models.Partner) error
+	GetPartnersByDriver(ctx context.Context) ([]models.Partner, error)
+	GetPartnersBySchool(ctx context.Context) ([]models.Partner, error)
+	DeletePartner(ctx context.Context) error
+}
+
+type PartnerRepository struct {
+	db *sql.DB
+}
+
+func NewPartnerRepository(db *sql.DB) *PartnerRepository {
+	return &PartnerRepository{
+		db: db,
+	}
+}
+
+func (pr *PartnerRepository) IsPartner(ctx context.Context, partner *models.Partner) (bool, error) {
+	return true, nil
+}
+
+func (pr *PartnerRepository) CreatePartners(ctx context.Context, partner *models.Partner) error {
+	return nil
+}
+
+func (pr *PartnerRepository) GetPartnersByDriver(ctx context.Context) {
+
+}
+
+func (pr *PartnerRepository) GetPartnersBySchool(ctx context.Context) {
+
+}
+
+func (pr *PartnerRepository) DeletePartner(ctx context.Context) {
+
+}
