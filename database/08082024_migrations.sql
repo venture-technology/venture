f2fc116074d2b10d4c9273d253e68f8c0a1e1c9e
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-08-07 22:35:32 -0300

    feat: migrations

--- database/08082024_migrations.sql
+++ database/08082024_migrations.sql
@@ -54,3 +54,15 @@ CREATE TABLE IF NOT EXISTS drivers (
     complement VARCHAR(10),
     zip VARCHAR(8) NOT NULL
 ); 
+
+-- Tabela de Convites
+CREATE TABLE IF NOT EXISTS invites (
+    invite_id SERIAL PRIMARY KEY,
+    requester VARCHAR(14), -- school
+    school VARCHAR(100) NOT NULL, --name_school
+    email_school VARCHAR(100) NOT NULL,
+    guest VARCHAR(14), -- driver
+    driver VARCHAR(100) NOT NULL, --name_driver
+    email_driver VARCHAR(100) NOT NULL,
+    status TEXT NOT NULL
+);
