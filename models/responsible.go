2e7414d08ed51d1f0525394683872e78bc497085
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2024-08-08 00:24:20 -0300

    feat: add omitempty

--- models/responsible.go
+++ models/responsible.go
@@ -3,20 +3,20 @@ package models
 import "github.com/dgrijalva/jwt-go"
 
 type Responsible struct {
-	ID            int        `json:"id"`
-	Name          string     `json:"name"`
-	Email         string     `json:"email"`
-	Password      string     `json:"password"`
-	CPF           string     `json:"cpf"`
-	Street        string     `json:"street"`
-	Number        string     `json:"number"`
-	ZIP           string     `json:"zip"`
-	Complement    string     `json:"complement"`
-	Status        string     `json:"status" validate:"oneof=OK ACTIVE BLOCKED BANNED"`
-	CreditCard    CreditCard `json:"card"`
-	PaymentMethod string     `json:"payment_method"`
-	CustomerId    string     `json:"customer_id"`
-	Phone         string     `json:"phone"`
+	ID              int        `json:"id"`
+	Name            string     `json:"name"`
+	Email           string     `json:"email"`
+	Password        string     `json:"password"`
+	CPF             string     `json:"cpf"`
+	Street          string     `json:"street"`
+	Number          string     `json:"number"`
+	ZIP             string     `json:"zip"`
+	Complement      string     `json:"complement"`
+	Status          string     `json:"status" validate:"oneof=OK ACTIVE BLOCKED BANNED"`
+	CreditCard      CreditCard `json:"card"`
+	CustomerId      string     `json:"customer_id,omitempty"`
+	PaymentMethodId string     `json:"payment_method_id,omitempty"`
+	Phone           string     `json:"phone"`
 }
 
 type CreditCard struct {
