e7041efc684115552e162259ce1f09fcc557f49b
gurodrigues-dev <gustavo.rodrigues@enjoei.com.br>
2024-08-12 16:36:57 -0300

    feat: implementing global headers

--- cmd/api/gateway/gateway.go
+++ cmd/api/gateway/gateway.go
@@ -1,1 +1,12 @@
 package gateway
+
+import "github.com/gin-gonic/gin"
+
+func NewGateway(group *gin.RouterGroup) {
+
+}
+
+func ConfigureHeaders(c *gin.Context) {
+	c.Header("Content-Type", "application/json; charset=utf-8")
+	c.Header("charset", "utf-8")
+}
