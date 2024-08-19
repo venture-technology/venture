5cef455a28c16531f7930f47307cc490f9125f62
gurodrigues-dev <gustavo.rodrigues@enjoei.com.br>
2024-08-19 00:59:45 -0300

    feat: create child handler

--- internal/handler/child_handler.go
+++ internal/handler/child_handler.go
@@ -1,1 +1,32 @@
 package handler
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/venture-technology/venture/internal/usecase/child"
+)
+
+type ChildHandler struct {
+	childUseCase *child.ChildUseCase
+}
+
+func NewChildHandler(childUseCase *child.ChildUseCase) *ChildHandler {
+	return &ChildHandler{
+		childUseCase: childUseCase,
+	}
+}
+
+func (ch *ChildHandler) Create(c *gin.Context) {
+
+}
+
+func (ch *ChildHandler) Get(c *gin.Context) {
+
+}
+
+func (ch *ChildHandler) Update(c *gin.Context) {
+
+}
+
+func (ch *ChildHandler) Delete(c *gin.Context) {
+
+}
