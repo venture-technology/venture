9de65ff0a6e7121b0d4ff07fd236f973cd594323
gurodrigues-dev <gustavo.rodrigues@enjoei.com.br>
2024-08-19 22:38:31 -0300

    refactor: add omitempty to handler message in child entity

--- internal/entity/child.go
+++ internal/entity/child.go
@@ -1,9 +1,9 @@
 package entity
 
 type Child struct {
-	ID          int         `json:"id"`
+	ID          int         `json:"id,omitempty"`
 	Name        string      `json:"name" validate:"required"`
 	RG          string      `json:"rg" validate:"required" example:"559378847"`
-	Responsible Responsible `json:"responsible" validate:"required"`
+	Responsible Responsible `json:"responsible,omitempty" validate:"required"`
 	Shift       string      `json:"shift" validate:"oneof='matutino' 'vespertino' 'noturno'"`
 }
