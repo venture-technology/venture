ecd850229aa5ff760b1674aff8e484bf73893e5a
NFAbricio <fabnunes014@gmail.com>
2024-08-19 23:37:13 -0300

    test: add test functions

--- internal/usecase/responsible/responsible_usecase_test.go
+++ internal/usecase/responsible/responsible_usecase_test.go
@@ -11,28 +11,42 @@ import (
 
 func TestResponsibleUseCase_Create(t *testing.T) {
 	mock := mocks.NewIResponsibleRepository(t)
-
 	mock.On("Create", context.Background(), &entity.Responsible{}).Return(nil)
-
 	service := NewResponsibleUseCase(mock)
-
 	err := service.Create(context.Background(), &entity.Responsible{})
-
 	assert.Nil(t, err)
-
 	mock.AssertExpectations(t)
 }
 
 func TestResponsibleUseCase_Get(t *testing.T) {
-
+	mock := mocks.NewIResponsibleRepository(t)
+	cpf := "12345678910"
+	mock.On("Get", context.Background(), &cpf).Return(&entity.Responsible{}, nil)
+	service := NewResponsibleUseCase(mock)
+	_, err := service.Get(context.Background(), &cpf)
+	assert.Nil(t, err)
+	mock.AssertExpectations(t)
 }
 
 func TestResponsibleUseCase_Upadte(t *testing.T) {
-
+	mock := mocks.NewIResponsibleRepository(t)
+	responsible := entity.Responsible{Name: "adsasd", CPF: "12345678910", Email: "jorge@gmail.com"}
+	Cresponsible := entity.Responsible{Name: "adsasd", CPF: "12345678910", Email: "jorge@gmail.com"}
+	mock.On("Update", context.Background(), &responsible, &Cresponsible).Return(nil)
+	service := NewResponsibleUseCase(mock)
+	err := service.Update(context.Background(), &responsible, &Cresponsible)
+	assert.Nil(t, err)
+	mock.AssertExpectations(t)
 }
 
 func TestResponsibleUseCase_Delete(t *testing.T) {
-
+	mock := mocks.NewIResponsibleRepository(t)
+	cpf := "12345678910"
+	mock.On("Delete", context.Background(), &cpf).Return(nil)
+	service := NewResponsibleUseCase(mock)
+	err := service.Delete(context.Background(), &cpf)
+	assert.Nil(t, err)
+	mock.AssertExpectations(t)
 }
 
 func TestResponsibleUseCase_SaveCard(t *testing.T) {
