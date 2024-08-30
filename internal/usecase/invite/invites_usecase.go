48db4ec21760781d7a286e06c940d7253bae6b99
gurodrigues-dev <gustavo.rodrigues@enjoei.com.br>
2024-08-30 13:15:41 -0300

    feat: invite implementation

---
+++ internal/usecase/invite/invites_usecase.go
@@ -0,0 +1,68 @@
+package invite
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/google/uuid"
+	"github.com/venture-technology/venture/internal/entity"
+	"github.com/venture-technology/venture/internal/repository"
+)
+
+type InviteUseCase struct {
+	inviteRepository  repository.IInviteRepository
+	partnerRepository repository.IPartnerRepository
+}
+
+func NewInviteUseCase(ir repository.IInviteRepository, pr repository.IPartnerRepository) *InviteUseCase {
+	return &InviteUseCase{
+		inviteRepository:  ir,
+		partnerRepository: pr,
+	}
+}
+
+func (iu *InviteUseCase) Create(ctx context.Context, invite *entity.Invite) error {
+
+	id, err := uuid.NewV7()
+	if err != nil {
+		return err
+	}
+
+	invite.ID = id
+
+	log.Print("validating partner")
+	isPartner, err := iu.partnerRepository.IsPartner(ctx, &invite.Driver.CNH, &invite.School.CNPJ)
+
+	if err != nil {
+		return err
+	}
+
+	if isPartner {
+		return fmt.Errorf("it wasnt created invite because they are partners")
+	}
+
+	log.Print("creating invite")
+	return iu.inviteRepository.Create(ctx, invite)
+
+}
+
+func (iu *InviteUseCase) Get(ctx context.Context, id uuid.UUID) (*entity.Invite, error) {
+	return iu.inviteRepository.Get(ctx, id)
+}
+
+func (iu *InviteUseCase) FindAllByCnh(ctx context.Context, cnh *string) ([]entity.Invite, error) {
+	return iu.inviteRepository.FindAllByCnh(ctx, cnh)
+}
+
+func (iu *InviteUseCase) FindAllByCnpj(ctx context.Context, cnpj *string) ([]entity.Invite, error) {
+	return iu.inviteRepository.FindAllByCnpj(ctx, cnpj)
+}
+
+func (iu *InviteUseCase) Accept(ctx context.Context, id uuid.UUID) error {
+	return iu.inviteRepository.Accept(ctx, id)
+}
+
+func (iu *InviteUseCase) Decline(ctx context.Context, id uuid.UUID) error {
+	return iu.inviteRepository.Decline(ctx, id)
+}
