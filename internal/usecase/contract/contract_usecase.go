398bd26c4ee48fa26b8cc2d99b427760e63e0523
gurodrigues-dev <gustavo.rodrigues@enjoei.com.br>
2024-09-13 11:01:12 -0300

    refactor: remove unused funcs in contract use case

--- internal/usecase/contract/contract_usecase.go
+++ internal/usecase/contract/contract_usecase.go
@@ -4,16 +4,9 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"time"
 
 	"github.com/google/uuid"
-	"github.com/stripe/stripe-go/v79"
-	"github.com/stripe/stripe-go/v79/invoice"
-	"github.com/stripe/stripe-go/v79/paymentintent"
-	"github.com/stripe/stripe-go/v79/price"
-	"github.com/stripe/stripe-go/v79/product"
-	"github.com/stripe/stripe-go/v79/subscription"
-	"github.com/venture-technology/venture/config"
+	"github.com/venture-technology/venture/internal/domain/payments"
 	"github.com/venture-technology/venture/internal/entity"
 	"github.com/venture-technology/venture/internal/repository"
 	"github.com/venture-technology/venture/internal/usecase"
@@ -24,14 +17,16 @@ type ContractUseCase struct {
 	childRepository    repository.IChildRepository
 	driverRepository   repository.IDriverRepository
 	schoolRepository   repository.ISchoolRepository
+	stripe             payments.IStripe
 }
 
-func NewContractUseCase(cou repository.IContractRepository, cr repository.IChildRepository, dr repository.IDriverRepository, sr repository.ISchoolRepository) *ContractUseCase {
+func NewContractUseCase(cou repository.IContractRepository, cr repository.IChildRepository, dr repository.IDriverRepository, sr repository.ISchoolRepository, st payments.IStripe) *ContractUseCase {
 	return &ContractUseCase{
 		contractRepository: cou,
 		childRepository:    cr,
 		driverRepository:   dr,
 		schoolRepository:   sr,
+		stripe:             st,
 	}
 }
 
@@ -76,21 +71,21 @@ func (cou *ContractUseCase) Create(ctx context.Context, contract *entity.Contrac
 
 	contract.Amount = usecase.CalculateContract(*distance, float64(contract.Driver.Amount))
 
-	prodt, err := CreateProduct(contract)
+	prodt, err := cou.stripe.CreateProduct(contract)
 	if err != nil {
 		return err
 	}
 
 	contract.StripeSubscription.ProductSubscriptionId = prodt.ID
 
-	pr, err := CreatePrice(contract)
+	pr, err := cou.stripe.CreatePrice(contract)
 	if err != nil {
 		return err
 	}
 
 	contract.StripeSubscription.PriceSubscriptionId = pr.ID
 
-	subs, err := CreateSubscription(contract)
+	subs, err := cou.stripe.CreateSubscription(contract)
 	if err != nil {
 		return err
 	}
@@ -121,7 +116,7 @@ func (cou *ContractUseCase) Get(ctx context.Context, id uuid.UUID) (*entity.Cont
 		return nil, err
 	}
 
-	invoices, err := ListInvoices(contract)
+	invoices, err := cou.stripe.ListInvoices(contract)
 	if err != nil {
 		return nil, err
 	}
@@ -147,7 +142,7 @@ func (cou *ContractUseCase) FindAllByCnh(ctx context.Context, cnh *string) ([]en
 
 	for _, contract := range contracts {
 
-		invoices, err := ListInvoices(&contract)
+		invoices, err := cou.stripe.ListInvoices(&contract)
 		if err != nil {
 			return nil, err
 		}
@@ -170,9 +165,9 @@ func (cou *ContractUseCase) Cancel(ctx context.Context, id uuid.UUID) error {
 		return err
 	}
 
-	values := CalculateRemainingValueSubscription(contract.Invoices)
+	values := cou.stripe.CalculateRemainingValueSubscription(contract.Invoices)
 
-	_, err = FineResponsible(contract, int64(values.Fines))
+	_, err = cou.stripe.FineResponsible(contract, int64(values.Fines))
 	if err != nil {
 		return err
 	}
@@ -181,7 +176,7 @@ func (cou *ContractUseCase) Cancel(ctx context.Context, id uuid.UUID) error {
 }
 
 func (cou *ContractUseCase) GetInvoice(ctx context.Context, invoice *string) (*entity.InvoiceInfo, error) {
-	inv, err := GetInvoice(*invoice)
+	inv, err := cou.stripe.GetInvoice(*invoice)
 	if err != nil {
 		return nil, err
 	}
@@ -196,199 +191,4 @@ func (cou *ContractUseCase) GetInvoice(ctx context.Context, invoice *string) (*e
 
 func (cou *ContractUseCase) Expired(ctx context.Context, id uuid.UUID) error {
 	return cou.contractRepository.Expired(ctx, id)
-}
-
-func CreateProduct(contract *entity.Contract) (*stripe.Product, error) {
-	conf := config.Get()
-
-	stripe.Key = conf.StripeEnv.SecretKey
-
-	params := &stripe.ProductParams{
-		Name:        stripe.String(fmt.Sprintf("Assinatura - Motorista: %s & Responsável: %s por: %s", contract.Driver.CNH, contract.Child.Responsible.CPF, contract.Child.RG)),
-		Description: stripe.String(fmt.Sprintf("Assinatura - Motorista: %s & Responsável: %s por: %s", contract.Driver.Name, contract.Child.Responsible.Name, contract.Child.Name)),
-	}
-
-	prodt, err := product.New(params)
-	if err != nil {
-		return nil, err
-	}
-
-	return prodt, nil
-}
-
-func CreatePrice(contract *entity.Contract) (*stripe.Price, error) {
-	conf := config.Get()
-
-	stripe.Key = conf.StripeEnv.SecretKey
-
-	params := &stripe.PriceParams{
-		Currency: stripe.String(string("brl")),
-		Product:  stripe.String(contract.StripeSubscription.ProductSubscriptionId),
-		Recurring: &stripe.PriceRecurringParams{
-			Interval: stripe.String("month"),
-		},
-		UnitAmount: stripe.Int64(int64(contract.Amount) * 100),
-	}
-
-	pr, err := price.New(params)
-	if err != nil {
-		return nil, err
-	}
-
-	return pr, nil
-}
-
-func CreateSubscription(contract *entity.Contract) (*stripe.Subscription, error) {
-	conf := config.Get()
-
-	stripe.Key = conf.StripeEnv.SecretKey
-
-	params := &stripe.SubscriptionParams{
-		CancelAt: stripe.Int64(time.Now().AddDate(0, 12, 0).Unix()),
-		Customer: stripe.String(contract.Child.Responsible.CustomerId),
-		Items: []*stripe.SubscriptionItemsParams{
-			{
-				Price: stripe.String(contract.StripeSubscription.PriceSubscriptionId),
-			},
-		},
-	}
-
-	subs, err := subscription.New(params)
-	if err != nil {
-		return nil, err
-	}
-
-	return subs, err
-}
-
-func GetSubscription(subscriptionId string) (*stripe.Subscription, error) {
-	conf := config.Get()
-
-	stripe.Key = conf.StripeEnv.SecretKey
-
-	subscription, err := subscription.Get(subscriptionId, nil)
-	if err != nil {
-		return nil, err
-	}
-	return subscription, nil
-}
-
-func ListSubscriptions(contract *entity.Contract) ([]entity.SubscriptionInfo, error) {
-	conf := config.Get()
-
-	stripe.Key = conf.StripeEnv.SecretKey
-
-	params := &stripe.SubscriptionListParams{
-		Customer: stripe.String(contract.Child.Responsible.CustomerId),
-	}
-	params.Filters.AddFilter("limit", "", "10")
-
-	var subscriptions []entity.SubscriptionInfo
-
-	i := subscription.List(params)
-
-	for i.Next() {
-		s := i.Subscription()
-		subscriptions = append(subscriptions, entity.SubscriptionInfo{
-			ID:     s.ID,
-			Status: string(s.Status),
-		})
-	}
-
-	if err := i.Err(); err != nil {
-		return nil, err
-	}
-
-	return subscriptions, nil
-}
-
-func DeleteSubscription(contract *entity.Contract) (*stripe.Subscription, error) {
-	conf := config.Get()
-
-	stripe.Key = conf.StripeEnv.SecretKey
-
-	deletedSub, err := subscription.Cancel(contract.StripeSubscription.SubscriptionId, nil)
-	if err != nil {
-		return nil, err
-	}
-	return deletedSub, nil
-}
-
-func GetInvoice(invoiceId string) (*stripe.Invoice, error) {
-	conf := config.Get()
-
-	stripe.Key = conf.StripeEnv.SecretKey
-
-	inv, err := invoice.Get(invoiceId, nil)
-	if err != nil {
-		return nil, err
-	}
-	return inv, nil
-}
-
-func ListInvoices(contract *entity.Contract) ([]entity.InvoiceInfo, error) {
-	conf := config.Get()
-
-	stripe.Key = conf.StripeEnv.SecretKey
-
-	params := &stripe.InvoiceListParams{
-		Subscription: stripe.String(contract.StripeSubscription.SubscriptionId),
-	}
-
-	var invoices []entity.InvoiceInfo
-
-	i := invoice.List(params)
-
-	for i.Next() {
-
-		charge := i.Invoice()
-		invoices = append(invoices, entity.InvoiceInfo{
-			ID:              charge.ID,
-			Status:          string(charge.Status),
-			AmountDue:       charge.AmountDue,
-			AmountRemaining: charge.AmountRemaining * 100,
-		})
-
-	}
-
-	if err := i.Err(); err != nil {
-		return nil, err
-	}
-
-	return invoices, nil
-}
-
-func CalculateRemainingValueSubscription(invoices []entity.InvoiceInfo) *entity.InvoiceRemaining {
-	invoice := entity.InvoiceRemaining{
-		InvoiceValue: float64(invoices[0].AmountDue / 100),
-		Quantity:     float64(len(invoices)),
-	}
-
-	invoice.Remaining = invoice.InvoiceValue * (12 - invoice.Quantity)
-
-	invoice.Fines = invoice.Remaining * 0.40
-
-	return &invoice
-}
-
-func FineResponsible(contract *entity.Contract, amountFine int64) (*stripe.PaymentIntent, error) {
-	conf := config.Get()
-
-	stripe.Key = conf.StripeEnv.SecretKey
-
-	params := &stripe.PaymentIntentParams{
-		Customer:      stripe.String(contract.Child.Responsible.CustomerId),
-		Amount:        stripe.Int64(amountFine * 100),
-		Currency:      stripe.String("brl"),
-		PaymentMethod: stripe.String(contract.Child.Responsible.PaymentMethodId),
-		OffSession:    stripe.Bool(true),
-		Confirm:       stripe.Bool(true),
-	}
-
-	paym, err := paymentintent.New(params)
-	if err != nil {
-		return nil, err
-	}
-
-	return paym, nil
 }
