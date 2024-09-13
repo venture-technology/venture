a5227f8e01b2ebf2d55cb0cf45d06418c2b4345c
gurodrigues-dev <gustavo.rodrigues@enjoei.com.br>
2024-09-12 23:48:59 -0300

    feat: auth tests

---
+++ internal/usecase/auth/auth_test.go
@@ -0,0 +1,48 @@
+package auth
+
+import (
+	"context"
+	"testing"
+
+	"github.com/venture-technology/venture/internal/entity"
+	"github.com/venture-technology/venture/mocks"
+)
+
+func TestAuthUsecase_LoginResponsible(t *testing.T) {
+	mock := mocks.NewIResponsibleRepository(t)
+	fakeResponsible := entity.Responsible{
+		Email: "teste@gmail.com",
+	}
+	mock.On("FindByEmail", context.Background(), &fakeResponsible.Email).Return(&entity.Responsible{}, nil)
+	service := NewAuthUseCase(nil, nil, mock)
+	_, err := service.LoginResponsible(context.Background(), &fakeResponsible.Email)
+	if err != nil {
+		t.Errorf("Error: %s", err)
+	}
+}
+
+func TestAuthUsecase_LoginDriver(t *testing.T) {
+	mock := mocks.NewIDriverRepository(t)
+	fakeDriver := entity.Driver{
+		Email: "teste@gmail.com",
+	}
+	mock.On("FindByEmail", context.Background(), &fakeDriver.Email).Return(&entity.Driver{}, nil)
+	service := NewAuthUseCase(nil, mock, nil)
+	_, err := service.LoginDriver(context.Background(), &fakeDriver.Email)
+	if err != nil {
+		t.Errorf("Error: %s", err)
+	}
+}
+
+func TestAuthUsecase_LoginSchool(t *testing.T) {
+	mock := mocks.NewISchoolRepository(t)
+	fakeSchool := entity.School{
+		Email: "teste@gmail.com",
+	}
+	mock.On("FindByEmail", context.Background(), &fakeSchool.Email).Return(&entity.School{}, nil)
+	service := NewAuthUseCase(mock, nil, nil)
+	_, err := service.LoginSchool(context.Background(), &fakeSchool.Email)
+	if err != nil {
+		t.Errorf("Error: %s", err)
+	}
+}
