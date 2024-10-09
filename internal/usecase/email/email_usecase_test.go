ec20a911006c288e97201e3c655bbf604efa869a
gurodrigues-dev <gustavo.rodrigues@enjoei.com.br>
2024-10-09 02:13:56 -0300

    feat : adding unit tests

---
+++ internal/usecase/email/email_usecase_test.go
@@ -0,0 +1,44 @@
+package email
+
+import (
+	"context"
+	"fmt"
+	"testing"
+
+	"github.com/venture-technology/venture/internal/entity"
+	"github.com/venture-technology/venture/mocks"
+)
+
+func TestEmailUseCase_Record(t *testing.T) {
+
+	t.Run("when return sucess", func(t *testing.T) {
+		caller := mocks.NewIEmailRepository(t)
+		email := entity.Email{
+			Body:      "body",
+			Subject:   "subject",
+			Recipient: "eu@omg.com",
+		}
+		caller.On("Record", context.Background(), &email).Return(nil)
+		useCase := NewEmailUseCase(caller, nil)
+		err := useCase.Record(context.Background(), &email)
+		if err != nil {
+			t.Errorf("Error: %s", err)
+		}
+	})
+
+	t.Run("when return error", func(t *testing.T) {
+		caller := mocks.NewIEmailRepository(t)
+		email := entity.Email{
+			Body:      "body",
+			Subject:   "subject",
+			Recipient: "eu@omg.com",
+		}
+		caller.On("Record", context.Background(), &email).Return(fmt.Errorf("error happens"))
+		useCase := NewEmailUseCase(caller, nil)
+		err := useCase.Record(context.Background(), &email)
+		if err == nil {
+			t.Error("Not returned error")
+		}
+	})
+
+}
