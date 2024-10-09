e5a839b219e84864603ed626e3aa47b829431794
gurodrigues-dev <gustavo.rodrigues@enjoei.com.br>
2024-10-09 02:43:55 -0300

    feat: add logger at auth

--- internal/usecase/auth/auth_test.go
+++ internal/usecase/auth/auth_test.go
@@ -14,7 +14,7 @@ func TestAuthUsecase_LoginResponsible(t *testing.T) {
 		Email: "teste@gmail.com",
 	}
 	mock.On("FindByEmail", context.Background(), &fakeResponsible.Email).Return(&entity.Responsible{}, nil)
-	service := NewAuthUseCase(nil, nil, mock)
+	service := NewAuthUseCase(nil, nil, mock, nil)
 	_, err := service.LoginResponsible(context.Background(), &fakeResponsible.Email)
 	if err != nil {
 		t.Errorf("Error: %s", err)
@@ -27,7 +27,7 @@ func TestAuthUsecase_LoginDriver(t *testing.T) {
 		Email: "teste@gmail.com",
 	}
 	mock.On("FindByEmail", context.Background(), &fakeDriver.Email).Return(&entity.Driver{}, nil)
-	service := NewAuthUseCase(nil, mock, nil)
+	service := NewAuthUseCase(nil, mock, nil, nil)
 	_, err := service.LoginDriver(context.Background(), &fakeDriver.Email)
 	if err != nil {
 		t.Errorf("Error: %s", err)
@@ -40,7 +40,7 @@ func TestAuthUsecase_LoginSchool(t *testing.T) {
 		Email: "teste@gmail.com",
 	}
 	mock.On("FindByEmail", context.Background(), &fakeSchool.Email).Return(&entity.School{}, nil)
-	service := NewAuthUseCase(mock, nil, nil)
+	service := NewAuthUseCase(mock, nil, nil, nil)
 	_, err := service.LoginSchool(context.Background(), &fakeSchool.Email)
 	if err != nil {
 		t.Errorf("Error: %s", err)
