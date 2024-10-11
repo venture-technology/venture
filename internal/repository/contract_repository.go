97ca94f98bcc931982c40d527609b8344b6e992e
gurodrigues-dev <gustavo.rodrigues@enjoei.com.br>
2024-10-11 00:50:53 -0300

    fix: solve error in query

--- internal/repository/contract_repository.go
+++ internal/repository/contract_repository.go
@@ -124,7 +124,7 @@ func (cr *ContractRepository) FindAllByRg(ctx context.Context, rg *string) (*ent
 		JOIN
 			children ch ON c.child_id = ch.rg
 		JOIN
-			responsibles r ON ch.responsible_id = r.cpf
+			responsible r ON ch.responsible_id = r.cpf
 		WHERE
 			ch.rg = $1;
 	`
