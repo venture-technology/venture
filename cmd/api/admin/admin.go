a8eded5c27bb6a3943bae981f584b045fd3266d9
gurodrigues-dev <gustavo.rodrigues@enjoei.com.br>
2024-12-29 15:07:38 -0300

    temp: comment admin.go

--- cmd/api/admin/admin.go
+++ cmd/api/admin/admin.go
@@ -1,39 +1,39 @@
 package admin
 
-import (
-	"github.com/venture-technology/venture/internal/handler"
-	"github.com/venture-technology/venture/internal/infra"
-	"github.com/venture-technology/venture/internal/repository"
-	"github.com/venture-technology/venture/internal/usecase/admin"
-	"github.com/venture-technology/venture/internal/usecase/auth/middleware"
-)
+// import (
+// 	"github.com/venture-technology/venture/internal/handler"
+// 	"github.com/venture-technology/venture/internal/infra"
+// 	"github.com/venture-technology/venture/internal/repository"
+// 	"github.com/venture-technology/venture/internal/usecase/admin"
+// 	"github.com/venture-technology/venture/internal/usecase/auth/middleware"
+// )
 
-type Admin struct {
-	app   *infra.Application
-	admin *handler.AdminHandler
-}
+// type Admin struct {
+// 	app   *infra.Application
+// 	admin *handler.AdminHandler
+// }
 
-func NewAdmin(app *infra.Application) *Admin {
-	return &Admin{
-		app: app,
-	}
-}
+// func NewAdmin(app *infra.Application) *Admin {
+// 	return &Admin{
+// 		app: app,
+// 	}
+// }
 
-func (adm *Admin) Setup() {
-	adm.app.Adm.Use(middleware.AdminMiddleware())
+// func (adm *Admin) Setup() {
+// 	adm.app.Adm.Use(middleware.AdminMiddleware())
 
-	adm.admin = handler.NewAdminHandler(
-		admin.NewAdminUseCase(
-			repository.NewAdminRepository(
-				adm.app.Cache,
-			),
-			adm.app.Logger,
-		),
-	)
+// 	adm.admin = handler.NewAdminHandler(
+// 		admin.NewAdminUseCase(
+// 			repository.NewAdminRepository(
+// 				adm.app.Cache,
+// 			),
+// 			adm.app.Logger,
+// 		),
+// 	)
 
-	adm.NewRoutes()
-}
+// 	adm.NewRoutes()
+// }
 
-func (adm *Admin) NewRoutes() {
-	adm.app.Adm.POST("/api-key/:name", adm.admin.NewApiKey)
-}
+// func (adm *Admin) NewRoutes() {
+// 	adm.app.Adm.POST("/api-key/:name", adm.admin.NewApiKey)
+// }
