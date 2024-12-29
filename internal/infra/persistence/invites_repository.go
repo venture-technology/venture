86f088d1ce9185a2aa36983052cbb23312834fad
gurodrigues-dev <gustavo.rodrigues@enjoei.com.br>
2024-12-29 15:26:45 -0300

    feat: invites repository

---
+++ internal/infra/persistence/invites_repository.go
@@ -0,0 +1,44 @@
+package persistence
+
+import (
+	"github.com/google/uuid"
+	"github.com/venture-technology/venture/internal/entity"
+	"github.com/venture-technology/venture/internal/infra/contracts"
+)
+
+type InviteRepositoryImpl struct {
+	Postgres contracts.PostgresIface
+}
+
+func (ir InviteRepositoryImpl) Create(invite *entity.Invite) error {
+	return ir.Postgres.Client().Create(invite).Error
+}
+
+func (ir InviteRepositoryImpl) Get(uuid uuid.UUID) (*entity.Invite, error) {
+	var invite entity.Invite
+	err := ir.Postgres.Client().Where("uuid = ?", uuid).First(&invite).Error
+	if err != nil {
+		return nil, err
+	}
+	return &invite, nil
+}
+
+func (ir InviteRepositoryImpl) FindAllByCnh(cnh string) ([]entity.Invite, error) {
+	var invites []entity.Invite
+	err := ir.Postgres.Client().Where("guester = ? AND status = 'pending'", cnh).Find(&invites).Error
+	return invites, err
+}
+
+func (ir InviteRepositoryImpl) FindAllByCnpj(cnpj string) ([]entity.Invite, error) {
+	var invites []entity.Invite
+	err := ir.Postgres.Client().Where("requester = ? AND status = 'pending'", cnpj).Find(&invites).Error
+	return invites, err
+}
+
+func (ir InviteRepositoryImpl) Accept(uuid uuid.UUID) error {
+	return ir.Postgres.Client().Model(&entity.Invite{}).Where("uuid = ?", uuid).Update("status", "accepted").Error
+}
+
+func (ir InviteRepositoryImpl) Decline(uuid uuid.UUID) error {
+	return ir.Postgres.Client().Model(&entity.Invite{}).Where("uuid = ?", uuid).Update("status", "declined").Error
+}
