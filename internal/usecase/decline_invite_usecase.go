c3359c646e9d843b38d19ac0bff44eb81b80ea8c
gurodrigues-dev <gustavo.rodrigues@enjoei.com.br>
2024-12-29 15:31:22 -0300

    feat: decline invite usecase

---
+++ internal/usecase/decline_invite_usecase.go
@@ -0,0 +1,26 @@
+package usecase
+
+import (
+	"github.com/google/uuid"
+	"github.com/venture-technology/venture/internal/infra/contracts"
+	"github.com/venture-technology/venture/internal/infra/persistence"
+)
+
+type DeclineInviteUseCase struct {
+	repositories *persistence.PostgresRepositories
+	logger       contracts.Logger
+}
+
+func NewDeclineInviteUseCase(
+	repositories *persistence.PostgresRepositories,
+	logger contracts.Logger,
+) *DeclineInviteUseCase {
+	return &DeclineInviteUseCase{
+		repositories: repositories,
+		logger:       logger,
+	}
+}
+
+func (diuc *DeclineInviteUseCase) DeclineInvite(id uuid.UUID) error {
+	return diuc.repositories.InviteRepository.Decline(id)
+}
