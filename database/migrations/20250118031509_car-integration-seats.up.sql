23bab2d18cd3523b8b336dbd568811794d104599
gurodrigues-dev <gustavo.rodrigues@enjoei.com.br>
2025-01-18 03:57:32 -0300

    feat: sql prepare to remaining feat

---
+++ database/migrations/20250118031509_car-integration-seats.up.sql
@@ -0,0 +1,9 @@
+ALTER TABLE drivers ADD COLUMN car_name VARCHAR(225);
+ALTER TABLE drivers ADD COLUMN car_year VARCHAR(4);
+ALTER TABLE drivers ADD COLUMN car_capacity VARCHAR(3);
+ALTER TABLE drivers ADD COLUMN schedule VARCHAR(3);
+ALTER TABLE drivers ADD COLUMN seats_remaining VARCHAR(3);
+ALTER TABLE drivers ADD COLUMN seats_morning VARCHAR(3);
+ALTER TABLE drivers ADD COLUMN seats_afternoon VARCHAR(3);
+ALTER TABLE drivers ADD COLUMN seats_night VARCHAR(3);
+
