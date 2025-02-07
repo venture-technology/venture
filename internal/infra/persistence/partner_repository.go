70054597c4f72ffbff3848c72bc9321f76168ec2
gurodrigues-dev <gustavo.rodrigues@enjoei.com.br>
2025-02-07 01:41:46 -0300

    refactor: change query to reach bussiness rules

--- internal/infra/persistence/partner_repository.go
+++ internal/infra/persistence/partner_repository.go
@@ -30,16 +30,66 @@ func (pr PartnerRepositoryImpl) Get(id string) (*entity.Partner, error) {
 
 func (pr PartnerRepositoryImpl) FindAllByCnpj(cnpj string) ([]entity.Partner, error) {
 	var partners []entity.Partner
+
 	err := pr.Postgres.Client().Where("school_id = ?", cnpj).Find(&partners).Error
-	return partners, err
+	if err != nil {
+		return nil, err
+	}
+
+	for i := range partners {
+		var driver entity.Driver
+		err := pr.Postgres.Client().Where("cnh = ?", partners[i].DriverID).First(&driver).Error
+		if err != nil {
+			continue
+		}
+		partners[i].Driver = driver
+	}
+
+	return partners, nil
 }
 
 func (pr PartnerRepositoryImpl) FindAllByCnh(cnh string) ([]entity.Partner, error) {
 	var partners []entity.Partner
+
 	err := pr.Postgres.Client().Where("driver_id = ?", cnh).Find(&partners).Error
-	return partners, err
+	if err != nil {
+		return nil, err
+	}
+
+	for i := range partners {
+		var school entity.School
+		err := pr.Postgres.Client().Where("cnpj = ?", partners[i].SchoolID).First(&school).Error
+		if err != nil {
+			continue
+		}
+		partners[i].School = school
+	}
+
+	return partners, nil
 }
 
 func (pr PartnerRepositoryImpl) Delete(id string) error {
-	return pr.Postgres.Client().Delete(&entity.Partner{}, "record = ?", id).Error
+	tx := pr.Postgres.Client().Begin()
+	if tx.Error != nil {
+		return tx.Error
+	}
+
+	var partner entity.Partner
+	if err := tx.Where("record = ?", id).First(&partner).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err := tx.Where("requester = ? AND guester = ?", partner.SchoolID, partner.DriverID).
+		Delete(&entity.Invite{}).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if err := tx.Delete(&entity.Partner{}, "record = ?", id).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return tx.Commit().Error
 }
