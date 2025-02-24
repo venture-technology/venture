4aacaad96c1e13e8ef1a1a8ab4147ea4c86d812b
gurodrigues-dev <gustavo.rodrigues@enjoei.com.br>
2025-02-24 01:53:16 -0300

    feat: add http route to controller events from dropbox

---
+++ internal/controller/webhook_controller.go
@@ -0,0 +1,40 @@
+package controller
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/venture-technology/venture/internal/domain/service/agreements"
+	"github.com/venture-technology/venture/internal/exceptions"
+	"github.com/venture-technology/venture/internal/infra"
+	"github.com/venture-technology/venture/internal/usecase"
+)
+
+type WebhookController struct {
+}
+
+func NewWebhookController() *WebhookController {
+	return &WebhookController{}
+}
+
+func (wh *WebhookController) PostV1WebhookEvents(httpContext *gin.Context) {
+	var eventWrapper agreements.EventWrapper
+
+	if err := httpContext.BindJSON(&eventWrapper); err != nil {
+		httpContext.JSON(http.StatusBadRequest, exceptions.InvalidBodyContentResponseError(err))
+	}
+
+	usecase := usecase.NewWebhookEventsUseCase(
+		&infra.App.Repositories,
+		infra.App.Logger,
+	)
+
+	_, err := usecase.Execute(eventWrapper)
+	if err != nil {
+		httpContext.JSON(http.StatusInternalServerError, exceptions.InternalServerResponseError(err, "error handling webhook event"))
+		return
+	}
+
+	httpContext.Header("Content-Type", "text/plain")
+	httpContext.String(http.StatusOK, "Hello API Event Received")
+}
