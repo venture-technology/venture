ef293816679583ebd26bed726b3a96407e7e2cca
Davi Oliveira <davi.oliveira3092@gmail.com>
2025-03-07 03:04:30 -0300

    feat: show pending temporary contracts for responsible

--- internal/domain/repository/temporary_contract_repository.go
+++ internal/domain/repository/temporary_contract_repository.go
@@ -8,6 +8,10 @@ import (
 type TempContractRepository interface {
 	Create(contract *entity.TempContract) error
 	Get(uuid uuid.UUID) (*entity.TempContract, error)
+	FindAllByResponsible(cpf *string) ([]entity.TempContract, error)
+	//para verificar se todos os objetos do contrato existem
+	//
+	//{responsible, driver e kid}
 	GetByEveryone(contract *entity.TempContract) (bool, error)
 	Expire(uuid uuid.UUID) error
 	Cancel(uuid uuid.UUID) error
