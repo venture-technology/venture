d77232348d2612be5ea797f2b2b24095a7e7d107
gurodrigues-dev <gustavo.rodrigues@enjoei.com.br>
2025-03-19 18:43:29 -0300

    feat: adding new event test

--- internal/usecase/webhook_signature_events_usecase_test.go
+++ internal/usecase/webhook_signature_events_usecase_test.go
@@ -1,79 +1,118 @@
 package usecase
 
-// func TestWebhookEventsUsecase_Execute(t *testing.T) {
-// 	t.Run("if event not exists", func(t *testing.T) {
-// 		logger := mocks.NewLogger(t)
-// 		agreementService := mocks.NewAgreementService(t)
+import (
+	"errors"
+	"testing"
 
-// 		event := agreements.EventWrapper{
-// 			Event: agreements.Event{
-// 				EventType: "not found",
-// 			},
-// 		}
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"github.com/venture-technology/venture/internal/domain/service/adapters"
+	"github.com/venture-technology/venture/internal/domain/service/agreements"
+	"github.com/venture-technology/venture/internal/infra/persistence"
+	"github.com/venture-technology/venture/mocks"
+)
 
-// 		usecase := NewWebhookEventsUseCase(
-// 			&persistence.PostgresRepositories{},
-// 			logger,
-// 			adapters.Adapters{
-// 				AgreementService: agreementService,
-// 			},
-// 		)
+func TestWebhookEventsUsecase_Execute(t *testing.T) {
+	t.Run("if event not exists", func(t *testing.T) {
+		logger := mocks.NewLogger(t)
+		agreementService := mocks.NewAgreementService(t)
 
-// 		resp, err := usecase.Execute(context.Background(), event)
+		event := agreements.EventWrapper{
+			Event: agreements.Event{
+				EventType: "not found",
+			},
+		}
 
-// 		assert.EqualError(t, err, "event type not used")
-// 		assert.Nil(t, resp)
-// 	})
+		usecase := NewWebhookSignatureEventsUseCase(
+			&persistence.PostgresRepositories{},
+			logger,
+			adapters.Adapters{
+				AgreementService: agreementService,
+			},
+		)
 
-// 	t.Run("receive callback test webhook and returns fail", func(t *testing.T) {
-// 		logger := mocks.NewLogger(t)
-// 		agreementService := mocks.NewAgreementService(t)
+		resp, err := usecase.Execute(&gin.Context{}, event)
 
-// 		event := agreements.EventWrapper{
-// 			Event: agreements.Event{
-// 				EventType: "callback_test",
-// 			},
-// 		}
+		assert.EqualError(t, err, "event type not used")
+		assert.Nil(t, resp)
+	})
 
-// 		usecase := NewWebhookEventsUseCase(
-// 			&persistence.PostgresRepositories{},
-// 			logger,
-// 			adapters.Adapters{
-// 				AgreementService: agreementService,
-// 			},
-// 		)
+	t.Run("receive callback test webhook and returns fail", func(t *testing.T) {
+		logger := mocks.NewLogger(t)
+		agreementService := mocks.NewAgreementService(t)
 
-// 		agreementService.On("HandleCallbackVerification").Return(false, errors.New("handle callback verification error"))
-// 		logger.On("Errorf", mock.Anything, mock.Anything)
+		event := agreements.EventWrapper{
+			Event: agreements.Event{
+				EventType: "callback_test",
+			},
+		}
 
-// 		_, err := usecase.Execute(context.Background(), event)
+		usecase := NewWebhookSignatureEventsUseCase(
+			&persistence.PostgresRepositories{},
+			logger,
+			adapters.Adapters{
+				AgreementService: agreementService,
+			},
+		)
 
-// 		assert.EqualError(t, err, "handle callback verification error")
-// 	})
+		agreementService.On("HandleCallbackVerification").Return(false, errors.New("handle callback verification error"))
+		logger.On("Errorf", mock.Anything, mock.Anything)
 
-// 	t.Run("receive callback test webhook and returns sucess", func(t *testing.T) {
-// 		logger := mocks.NewLogger(t)
-// 		agreementService := mocks.NewAgreementService(t)
+		_, err := usecase.Execute(&gin.Context{}, event)
 
-// 		event := agreements.EventWrapper{
-// 			Event: agreements.Event{
-// 				EventType: "callback_test",
-// 			},
-// 		}
+		assert.EqualError(t, err, "handle callback verification error")
+	})
 
-// 		usecase := NewWebhookEventsUseCase(
-// 			&persistence.PostgresRepositories{},
-// 			logger,
-// 			adapters.Adapters{
-// 				AgreementService: agreementService,
-// 			},
-// 		)
+	t.Run("receive signature request all signed webhook and returns fail", func(t *testing.T) {
+		logger := mocks.NewLogger(t)
+		agreementService := mocks.NewAgreementService(t)
 
-// 		agreementService.On("HandleCallbackVerification").Return(true, nil)
-// 		logger.On("Infof", mock.Anything, mock.Anything)
+		event := agreements.EventWrapper{
+			Event: agreements.Event{
+				EventType: "signature_request_all_signed",
+			},
+		}
 
-// 		_, err := usecase.Execute(context.Background(), event)
+		usecase := NewWebhookSignatureEventsUseCase(
+			&persistence.PostgresRepositories{},
+			logger,
+			adapters.Adapters{
+				AgreementService: agreementService,
+			},
+		)
 
-// 		assert.Nil(t, err)
-// 	})
-// }
+		agreementService.On("SignatureRequestAllSigned", mock.Anything).Return(agreements.ASRASOutput{}, errors.New("signature request all signed error"))
+		logger.On("Errorf", mock.Anything, mock.Anything)
+
+		_, err := usecase.Execute(&gin.Context{}, event)
+
+		assert.EqualError(t, err, "signature request all signed error")
+	})
+
+	t.Run("receive callback test webhook and returns sucess", func(t *testing.T) {
+		logger := mocks.NewLogger(t)
+		agreementService := mocks.NewAgreementService(t)
+
+		event := agreements.EventWrapper{
+			Event: agreements.Event{
+				EventType: "callback_test",
+			},
+		}
+
+		usecase := NewWebhookSignatureEventsUseCase(
+			&persistence.PostgresRepositories{},
+			logger,
+			adapters.Adapters{
+				AgreementService: agreementService,
+			},
+		)
+
+		agreementService.On("HandleCallbackVerification").Return(true, nil)
+		logger.On("Infof", mock.Anything, mock.Anything)
+
+		_, err := usecase.Execute(&gin.Context{}, event)
+
+		assert.Nil(t, err)
+	})
+}
