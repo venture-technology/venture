b81a8511215f061c047490a73e3485361b42458e
gurodrigues-dev <gustavo.rodrigues@enjoei.com.br>
2025-03-18 22:11:31 -0300

    feat: add new event of signed contracts

--- internal/usecase/webhook_events_usecase.go
+++ internal/usecase/webhook_events_usecase.go
@@ -3,6 +3,7 @@ package usecase
 import (
 	"fmt"
 
+	"github.com/gin-gonic/gin"
 	"github.com/venture-technology/venture/internal/domain/service/adapters"
 	"github.com/venture-technology/venture/internal/domain/service/agreements"
 	"github.com/venture-technology/venture/internal/infra/contracts"
@@ -27,19 +28,41 @@ func NewWebhookEventsUseCase(
 	}
 }
 
-func (weuc *WebhookEventsUseCase) Execute(eventWrapper agreements.EventWrapper) (any, error) {
-	var eventHandlers = map[string]func() (any, error){
-		"callback_test": func() (any, error) {
+func (weuc *WebhookEventsUseCase) Execute(
+	httpCtx *gin.Context,
+	eventWrapper agreements.EventWrapper,
+) (any, error) {
+	var eventHandlers = map[string]func(httpCtx *gin.Context) (any, error){
+		"callback_test": func(httpCtx *gin.Context) (any, error) {
 			return weuc.adapters.AgreementService.HandleCallbackVerification()
+		},
+		"signature_request_all_signed": func(httpCtx *gin.Context) (any, error) {
+			asras, err := weuc.adapters.AgreementService.SignatureRequestAllSigned(httpCtx)
+			if err != nil {
+				return agreements.ASRASOutput{}, err
+			}
+
+			uc := NewAcceptContractUseCase(
+				weuc.repositories,
+				weuc.logger,
+				weuc.adapters,
+			)
+
+			err = uc.AcceptContract(asras)
+			if err != nil {
+				return agreements.ASRASOutput{}, err
+			}
+
+			return asras, nil
 		},
 	}
 
 	eventType := eventWrapper.Event.EventType
 
 	if handler, exists := eventHandlers[eventType]; exists {
-		response, err := handler()
+		response, err := handler(httpCtx)
 		if err != nil {
-			weuc.logger.Errorf(fmt.Sprintf("error handling event: %s", err))
+			weuc.logger.Errorf(fmt.Sprintf("error handling event: %s", eventType))
 			return nil, err
 		}
 		weuc.logger.Infof(fmt.Sprintf("event handled: %s, %s", response, eventType))
