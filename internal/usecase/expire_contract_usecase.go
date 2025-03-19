182d383bfb920fbddc518d7c4c2d2dd906c586a2
gurodrigues-dev <gustavo.rodrigues@enjoei.com.br>
2025-03-19 18:46:36 -0300

    chore: remove expire contract temp

--- internal/usecase/expire_contract_usecase.go
+++ internal/usecase/expire_contract_usecase.go
@@ -2,31 +2,30 @@ package usecase
 
 import (
 	"github.com/google/uuid"
-	"github.com/venture-technology/venture/internal/entity"
 	"github.com/venture-technology/venture/internal/infra/contracts"
 	"github.com/venture-technology/venture/internal/infra/persistence"
 )
 
-var expireSeat = map[string]func(ecuc *ExpireContractUseCase, contract *entity.Contract) error{
-	"morning": func(ecuc *ExpireContractUseCase, contract *entity.Contract) error {
-		return ecuc.repositories.DriverRepository.Update(contract.Driver.CNH, map[string]interface{}{
-			"seats_remaining": contract.Driver.Seats.Remaining + 1,
-			"seats_morning":   contract.Driver.Seats.Morning + 1,
-		})
-	},
-	"afternoon": func(ecuc *ExpireContractUseCase, contract *entity.Contract) error {
-		return ecuc.repositories.DriverRepository.Update(contract.Driver.CNH, map[string]interface{}{
-			"seats_remaining": contract.Driver.Seats.Remaining + 1,
-			"seats_afternoon": contract.Driver.Seats.Afternoon + 1,
-		})
-	},
-	"night": func(ecuc *ExpireContractUseCase, contract *entity.Contract) error {
-		return ecuc.repositories.DriverRepository.Update(contract.Driver.CNH, map[string]interface{}{
-			"seats_remaining": contract.Driver.Seats.Remaining + 1,
-			"seats_night":     contract.Driver.Seats.Night + 1,
-		})
-	},
-}
+// var expireSeat = map[string]func(ecuc *ExpireContractUseCase, contract *entity.Contract) error{
+// 	"morning": func(ecuc *ExpireContractUseCase, contract *entity.Contract) error {
+// 		return ecuc.repositories.DriverRepository.Update(contract.Driver.CNH, map[string]interface{}{
+// 			"seats_remaining": contract.Driver.Seats.Remaining + 1,
+// 			"seats_morning":   contract.Driver.Seats.Morning + 1,
+// 		})
+// 	},
+// 	"afternoon": func(ecuc *ExpireContractUseCase, contract *entity.Contract) error {
+// 		return ecuc.repositories.DriverRepository.Update(contract.Driver.CNH, map[string]interface{}{
+// 			"seats_remaining": contract.Driver.Seats.Remaining + 1,
+// 			"seats_afternoon": contract.Driver.Seats.Afternoon + 1,
+// 		})
+// 	},
+// 	"night": func(ecuc *ExpireContractUseCase, contract *entity.Contract) error {
+// 		return ecuc.repositories.DriverRepository.Update(contract.Driver.CNH, map[string]interface{}{
+// 			"seats_remaining": contract.Driver.Seats.Remaining + 1,
+// 			"seats_night":     contract.Driver.Seats.Night + 1,
+// 		})
+// 	},
+// }
 
 type ExpireContractUseCase struct {
 	repositories *persistence.PostgresRepositories
@@ -44,15 +43,16 @@ func NewExpireContractUseCase(
 }
 
 func (ecuc *ExpireContractUseCase) ExpireContract(uuid uuid.UUID) error {
-	contract, err := ecuc.repositories.ContractRepository.Get(uuid)
-	if err != nil {
-		return err
-	}
+	// contract, err := ecuc.repositories.ContractRepository.Get(uuid)
+	// if err != nil {
+	// 	return err
+	// }
 
-	err = ecuc.repositories.ContractRepository.Expired(uuid)
-	if err != nil {
-		return err
-	}
+	// err = ecuc.repositories.ContractRepository.Expired(uuid)
+	// if err != nil {
+	// 	return err
+	// }
 
-	return expireSeat[contract.Kid.Shift](ecuc, contract)
+	// return expireSeat[contract.Kid.Shift](ecuc, contract)
+	return nil
 }
