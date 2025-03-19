6dc3ea7ec40234b083f67f7c585c44a02f07830d
gurodrigues-dev <gustavo.rodrigues@enjoei.com.br>
2025-03-18 22:16:18 -0300

    feat: new routes for temp contracts

--- cmd/api/server/routes/v1/main.go
+++ cmd/api/server/routes/v1/main.go
@@ -34,51 +34,61 @@ func NewV1Controller() *V1Controllers {
 }
 
 func (route *V1Controllers) V1Routes(group *gin.RouterGroup) {
+	// responsible
 	group.POST("/responsible", route.Responsible.PostV1CreateResponsible)
 	group.GET("/responsible/:cpf", route.Responsible.GetV1GetResponsible)
 	group.PATCH("/responsible/:cpf", route.Responsible.PatchV1UpdateResponsible)
 	group.DELETE("/responsible/:cpf", route.Responsible.DeleteV1DeleteResponsbile)
 
+	// kid
 	group.POST("/kid", route.Kid.PostV1CreateKid)
 	group.GET("/kid/:rg", route.Kid.GetV1GetKid)
 	group.GET("/:cpf/kid", route.Kid.GetV1ListKids)
 	group.PATCH("/kid/:rg", route.Kid.PatchV1UpdateController)
 	group.DELETE("/kid/:rg", route.Kid.DeleteV1DeleteKid)
 
+	// school
 	group.POST("/school", route.School.PostV1CreateSchool)
 	group.GET("/school", route.School.GetV1ListSchool)
 	group.GET("/school/:cnpj", route.School.GetV1GetSchool)
 	group.PATCH("/school/:cnpj", route.School.PatchV1UpdateSchool)
 	group.DELETE("/school/:cnpj", route.School.DeleteV1DeleteSchool)
 
+	// driver
 	group.POST("/driver", route.Driver.PostV1Create)
 	group.GET("/driver/:cnh", route.Driver.GetV1GetDriver)
 	group.PATCH("/driver/:cnh", route.Driver.PatchV1UpdateDriver)
 	group.DELETE("/driver/:cnh", route.Driver.DeleteV1DeleteDriver)
 
+	// invite
 	group.POST("/invite", route.Invite.PostV1SendInvite)
 	group.GET("/driver/invite/:cnh", route.Invite.GetV1DriverListInvite)
 	group.GET("/school/invite/:cnpj", route.Invite.GetV1SchoolListInvite)
 	group.PATCH("/invite/:id/accept", route.Invite.PatchV1AcceptInvite)
 	group.DELETE("/invite/:id/decline", route.Invite.DeleteV1DeclineInvite)
 
+	// partner
 	group.GET("/driver/partner/:cnh", route.Partner.GetV1DriverListPartners)
 	group.GET("/school/partner/:cnpj", route.Partner.GetV1SchoolListPartners)
 	group.DELETE("/partner/:id", route.Partner.DeleteV1DeletePartner)
 
+	// contract
 	group.POST("/contract", route.Contract.PostV1CreateContract)
-	group.GET("/contract/:id", route.Contract.GetV1GetContract)
-	group.GET("/driver/contract/:cnh", route.Contract.GetV1ListDriverContract)
-	group.GET("/school/contract/:cnpj", route.Contract.GetV1ListContractSchool)
-	group.GET("/responsible/contract/:cpf", route.Contract.GetV1ListResponsibleContract)
-	group.PATCH("/contract/:id/cancel", route.Contract.PatchV1CancelContract)
-	group.PATCH("/contract/:id/expired", route.Contract.PatchV1ExpiredContract)
+	// group.GET("/contract/:id", route.Contract.GetV1GetContract)
+	// group.GET("/driver/contract/:cnh", route.Contract.GetV1ListDriverContract)
+	// group.GET("/school/contract/:cnpj", route.Contract.GetV1ListContractSchool)
+	// group.GET("/responsible/contract/:cpf", route.Contract.GetV1ListResponsibleContract)
+	group.PATCH("/contract/:id/cancel", route.Contract.PostV1CancelContract)
 
+	// webhook
 	group.POST("/webhook/signature/events", route.Webhook.PostV1WebhookEvents)
+	group.POST("/webhook/payments/events")
 
+	// price
 	group.GET("/price/:cpf/:cnpj", route.Price.GetV1PriceDriver)
 
+	// temporary contract
 	group.GET("/temp_contracts/responsible/:cpf", route.TemporaryContract.GetV1ResponsibleTempContracts)
 	group.GET("/temp_contracts/driver/:cnh", route.TemporaryContract.GetV1DriverTempContracts)
-
+	group.POST("/temp_contracts/cancel/:uuid", route.TemporaryContract.PostV1CancelTempContracts)
 }
