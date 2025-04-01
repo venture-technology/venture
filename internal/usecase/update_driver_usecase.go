cea227b4f7b7dac6277a5bfe3e9870e175d44ae9
gurodrigues-dev <gustavo.rodrigues@enjoei.com.br>
2025-04-01 17:12:31 -0300

    feat: update driver validate

--- internal/usecase/update_driver_usecase.go
+++ internal/usecase/update_driver_usecase.go
@@ -1,6 +1,9 @@
 package usecase
 
 import (
+	"fmt"
+
+	"github.com/venture-technology/venture/internal/entity"
 	"github.com/venture-technology/venture/internal/infra/contracts"
 	"github.com/venture-technology/venture/internal/infra/persistence"
 	"github.com/venture-technology/venture/internal/value"
@@ -23,10 +26,41 @@ func NewUpdateDriverUseCase(
 }
 
 func (uduc *UpdateDriverUseCase) UpdateDriver(cnh string, attributes map[string]interface{}) error {
-	err := utils.ValidateUpdate(attributes, value.DriverAllowedKeys)
+	driver, err := uduc.repositories.DriverRepository.Get(cnh)
+	if err != nil {
+		return err
+	}
+
+	err = utils.ValidateUpdate(attributes, value.DriverAllowedKeys)
+	if err != nil {
+		return err
+	}
+
+	err = uduc.validateProvincy(driver, attributes)
 	if err != nil {
 		return err
 	}
 
 	return uduc.repositories.DriverRepository.Update(cnh, attributes)
+}
+
+func (uduc *UpdateDriverUseCase) validateProvincy(driver *entity.Driver, attrs map[string]interface{}) error {
+	_, exists := attrs["states"]
+	if exists {
+		_, ok := attrs["states"].(string)
+		if !ok {
+			return fmt.Errorf("states type error")
+		}
+
+		hasContract, err := uduc.repositories.ContractRepository.DriverHasEnableContract(driver.CNH)
+		if err != nil {
+			return err
+		}
+
+		if hasContract {
+			return fmt.Errorf("impossible change provincy when has enable contract")
+		}
+	}
+
+	return nil
 }
