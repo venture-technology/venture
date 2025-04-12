e77db96afedd6720b001eae76e5e68ae61b20f0e
Davi Oliveira <davi.oliveira3092@gmail.com>
2025-04-12 01:45:24 -0300

    test: fixed unit tests

--- internal/usecase/cancel_temp_contract_usecase_test.go
+++ internal/usecase/cancel_temp_contract_usecase_test.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"testing"
 
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/venture-technology/venture/internal/infra/persistence"
@@ -11,6 +12,8 @@ import (
 )
 
 func TestCancelTempContractUsecase_CancelContract(t *testing.T) {
+	var uuid uuid.UUID
+
 	t.Run("when repository return error", func(t *testing.T) {
 		tempContractRepository := mocks.NewTempContractRepository(t)
 		logger := mocks.NewLogger(t)
@@ -24,7 +27,7 @@ func TestCancelTempContractUsecase_CancelContract(t *testing.T) {
 
 		tempContractRepository.On("Cancel", mock.Anything).Return(errors.New("database error"))
 
-		err := uc.CancelTempContract("123")
+		err := uc.CancelTempContract(uuid)
 
 		assert.EqualError(t, err, "database error")
 		assert.Error(t, err)
@@ -43,7 +46,7 @@ func TestCancelTempContractUsecase_CancelContract(t *testing.T) {
 
 		tempContractRepository.On("Cancel", mock.Anything).Return(nil)
 
-		err := uc.CancelTempContract("123")
+		err := uc.CancelTempContract(uuid)
 
 		assert.Nil(t, err)
 		assert.NoError(t, err)
