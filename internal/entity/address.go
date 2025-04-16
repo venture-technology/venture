d31ee1f076ed887ac8a18a3fd2ad60c01d3de270
gurodrigues-dev <gustavorodrigueslima2004@gmail.com>
2025-04-16 07:02:44 -0300

    feat: new address function

--- internal/entity/address.go
+++ internal/entity/address.go
@@ -1,8 +1,38 @@
 package entity
 
+import "fmt"
+
 type Address struct {
-	Street     string `json:"street,omitempty" validate:"required"`
-	Number     string `json:"number,omitempty" validate:"required"`
-	Zip        string `json:"zip,omitempty" validate:"required"`
-	Complement string `json:"complement,omitempty"`
+	Street       string `json:"street,omitempty" validate:"required"`
+	Number       string `json:"number,omitempty" validate:"required"`
+	Zip          string `json:"zip,omitempty" validate:"required"`
+	Complement   string `json:"complement,omitempty"`
+	City         string `json:"city,omitempty" validate:"required"`
+	State        string `json:"provincy,omitempty" validate:"required"`
+	Neighborhood string `json:"neighborhood,omitempty" validate:"required"`
+}
+
+func (a Address) GetFullAddress() string {
+	if a.Complement == "" {
+		return fmt.Sprintf(
+			"%s, %s - %s, %s - %s, %s",
+			a.Street,
+			a.Number,
+			a.Neighborhood,
+			a.City,
+			a.State,
+			a.Zip,
+		)
+	}
+
+	return fmt.Sprintf(
+		"%s, %s - %s, %s (%s) - %s, %s",
+		a.Street,
+		a.Number,
+		a.Complement,
+		a.Neighborhood,
+		a.City,
+		a.State,
+		a.Zip,
+	)
 }
