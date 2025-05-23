d260e9ef8e7a011d867754c14ebcc4b89f6a2a92
Davi Oliveira <davi.oliveira3092@gmail.com>
2025-05-23 14:51:00 -0300

    feat: microservice

---
+++ cmd/api/administrator/routes/admin/main.go
@@ -0,0 +1,42 @@
+package admin
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/venture-technology/venture/internal/controller"
+)
+
+type AdminControllers struct {
+	Event      *controller.EventController
+	Integrator *controller.IntegratorController
+	Admin      *controller.AdminController
+}
+
+func NewAdminController() *AdminControllers {
+	return &AdminControllers{
+		Event:      controller.NewEventController(),
+		Integrator: controller.NewIntegratorController(),
+		Admin:      controller.NewAdminController(),
+	}
+}
+
+func (route *AdminControllers) AdminRoutes(group *gin.RouterGroup) {
+
+	// Event routes
+	group.POST("/events", route.Event.PostV1CreateEvent)
+	group.GET("/events", route.Event.GetV1ListEvents)
+	group.PATCH("/events", route.Event.PatchV1UpdateEvent)
+	group.DELETE("/events", route.Event.DeleteV1DeleteEvent)
+	group.GET("/integrator/:id/events", route.Event.GetV1ListEventsByIntegrator)
+
+	// Admin routes
+	group.POST("/users", route.Admin.PostV1CreateAdmin)
+	group.PATCH("/users", route.Admin.PatchV1UpdateAdmin)
+	group.DELETE("/users", route.Admin.DeleteV1DeleteAdmin)
+	group.POST("/users/login", route.Admin.PostV1LoginAdmin)
+
+	// Integrator routes
+	group.POST("/integrators", route.Integrator.PostV1CreateIntegrator)
+	group.GET("/integrators", route.Integrator.GetV1ListIntegrators)
+	group.PATCH("/integrators", route.Integrator.PatchV1UpdateIntegrator)
+	group.DELETE("/integrators", route.Integrator.DeleteV1DeleteIntegrator)
+}
